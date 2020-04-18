cf9071c964cfaed16019a2273508f71197e1883a
Robyortega <roby.17421059@student.ubl.ac.id>
2020-04-18 13:45:28 +0700

    TugasHalamRegis

---
+++ app/src/main/java/com/robyortega/loginregister/users/RegistrasiActivity.java
@@ -0,0 +1,18 @@
+package com.robyortega.loginregister.users;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.robyortega.loginregister.R;
+
+public class RegistrasiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrasi);
+
+        getSupportActionBar().hide();
+    }
+}
